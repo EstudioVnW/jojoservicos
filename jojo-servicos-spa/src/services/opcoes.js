3efa3438d35eba8c3e5851a289f40cf2cb78483d
James da Silva <jamesodas@gmail.com>
2018-10-29 11:50:47 -0300

    update

--- jojo-servicos-spa/src/services/opcoes.js
+++ jojo-servicos-spa/src/services/opcoes.js
@@ -31,7 +31,7 @@ opcoes['exibir-opcoes-docs'].push(
   },
   {
     label: 'Boletim de Ocorrência',
-    evento: 'boletim-de-ocorrencia'
+    evento: 'agendar-boletim-de-ocorrencia'
   }
 );
 
@@ -66,5 +66,123 @@ opcoes['exibir-opcoes-ident-seg-via-motivos'].push(
     evento: 'ident-seg-via-mudou-civil'
   }
 );
+
+opcoes['exibir-opcoes-ctps-qual-via'] = [];
+opcoes['exibir-opcoes-ctps-qual-via'].push(
+  {
+    label: 'Primeira Via',
+    evento: 'agendar-ctps-primeira-via'
+  },
+  {
+    label: 'Segunda Via',
+    evento: 'agendar-ctps-segunda-via'
+  }
+);
+
+opcoes['exibir-opcoes-titulo-qual-via'] = [];
+opcoes['exibir-opcoes-titulo-qual-via'].push(
+  {
+    label: 'Primeira Via',
+    evento: 'agendar-titulo-primeira-via'
+  },
+  {
+    label: 'Segunda Via',
+    evento: 'agendar-titulo-segunda-via'
+  }
+);
+
+opcoes['exibir-opcoes-tem-mei'] = [];
+opcoes['exibir-opcoes-tem-mei'].push(
+  {
+    label: 'Sim',
+    evento: 'tem-mei-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'tem-mei-nao'
+  }
+);
+
+opcoes['exibir-opcoes-quer-dar-entrada-no-mei'] = [];
+opcoes['exibir-opcoes-quer-dar-entrada-no-mei'].push(
+  {
+    label: 'Sim',
+    evento: 'quer-dar-entrada-no-mei-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'quer-dar-entrada-no-mei-nao'
+  }
+);
+
+opcoes['exibir-opcoes-servicos-mei'] = [];
+opcoes['exibir-opcoes-servicos-mei'].push(
+  {
+    label: 'Alvará',
+    evento: 'agendar-alvara'
+  },
+  {
+    label: 'Registro de Vigilância Sanitária',
+    evento: 'agendar-registro-de-vigilancia-sanitaria'
+  },
+  {
+    label: 'Emitir nota',
+    evento: 'agendar-emitir-nota'
+  },
+  {
+    label: 'Negociação de débitos',
+    evento: 'agendar-negociacao-de-debitos'
+  }
+);
+
+
+opcoes['exibir-opcoes-primeiro-emprego'] = [];
+opcoes['exibir-opcoes-primeiro-emprego'].push(
+  {
+    label: 'Sim',
+    evento: 'primeiro-emprego-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'primeiro-emprego-nao'
+  }
+);
+
+opcoes['exibir-opcoes-emprego-idade'] = [];
+opcoes['exibir-opcoes-emprego-idade'].push(
+  {
+    label: 'Sim',
+    evento: 'menor-idade-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'menor-idade-nao'
+  }
+);
+
+opcoes['exibir-opcoes-completou-ensino-medio'] = [];
+opcoes['exibir-opcoes-completou-ensino-medio'].push(
+  {
+    label: 'Sim',
+    evento: 'completou-ensino-medio-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'completou-ensino-medio-nao'
+  }
+);
+
+opcoes['exibir-opcoes-outro-pedido'] = [];
+opcoes['exibir-opcoes-outro-pedido'].push(
+  {
+    label: 'Sim',
+    evento: 'outro-pedido-sim'
+  },
+  {
+    label: 'Não',
+    evento: 'outro-pedido-nao'
+  }
+);
+
 
 export default opcoes;
